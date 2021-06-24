8461847b6b537bd714543621d40ad3e2f91d5a48
Bre Thunholm <bthunholm@gmail.com>
2021-06-24 10:44:24 -0500

    set up action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POST'
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+
+export const fetchPosts =  () => async dispatch =>  {
+        const res = await jsonPlaceholder.get('/posts');
+
+        dispatch({ type: 'FETCH_POSTS', payload: res})
     }
-}
+   
+
+
